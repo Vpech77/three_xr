249702facb20f93cf87b2a550846df7c6bb0db37
vpech77 <vanessa.pech77@gmail.com>
2025-02-26 15:32:48 +0100

    clean

--- main.js
+++ main.js
@@ -1,50 +1,15 @@
 "use strict";
 
-// Import only what you need, to help your bundler optimize final code size using tree shaking
-// see https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking)
-
 import {
-  AmbientLight,
-  BoxGeometry,
   Clock,
-  Color,
-  CylinderGeometry,
-  HemisphereLight,
-  Mesh,
-  MeshNormalMaterial,
-  MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
   PMREMGenerator
 } from 'three';
 
-// XR Emulator
-import { DevUI } from '@iwer/devui';
-import { XRDevice, metaQuest3 } from 'iwer';
 
-// XR
 import { XRButton } from 'three/addons/webxr/XRButton.js';
-
-// If you prefer to import the whole library, with the THREE prefix, use the following line instead:
-// import * as THREE from 'three'
-
-// NOTE: three/addons alias is supported by Rollup: you can use it interchangeably with three/examples/jsm/  
-
-// Importing Ammo can be tricky.
-// Vite supports webassembly: https://vitejs.dev/guide/features.html#webassembly
-// so in theory this should work:
-//
-// import ammoinit from 'three/addons/libs/ammo.wasm.js?init';
-// ammoinit().then((AmmoLib) => {
-//  Ammo = AmmoLib.exports.Ammo()
-// })
-//
-// But the Ammo lib bundled with the THREE js examples does not seem to export modules properly.
-// A solution is to treat this library as a standalone file and copy it using 'vite-plugin-static-copy'.
-// See vite.config.js
-// 
-// Consider using alternatives like Oimo or cannon-es
 
 import {
   GLTFLoader
@@ -52,12 +17,6 @@ import {
 
 import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
-// Example of hard link to official repo for data, if needed
-// const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r173/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
-
-
-
-// INSERT CODE HERE
 let camera, scene, renderer;
 let controller;
 
@@ -65,13 +24,10 @@ const objects = [];
 
 const clock = new Clock();
 
-// Main loop
 const animate = () => {
 
   const delta = clock.getDelta();
   const elapsed = clock.getElapsedTime();
-
-  // can be used in shaders: uniforms.u_time.value = elapsed;
 
   renderer.render(scene, camera);
 };
@@ -82,47 +38,26 @@ const init = () => {
 
   camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
-  // const light = new AmbientLight(0xffffff, 1.0); // soft white light
-  // scene.add(light);
-
-  // const hemiLight = new HemisphereLight(0xffffff, 0xbbbbff, 3);
-  // hemiLight.position.set(0.5, 1, 0.25);
-  // scene.add(hemiLight);
-
   renderer = new WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate); // requestAnimationFrame() replacement, compatible with XR 
+  renderer.setAnimationLoop(animate);
   renderer.xr.enabled = true;
   document.body.appendChild(renderer.domElement);
-
-  /*
-  document.body.appendChild( XRButton.createButton( renderer, {
-    'optionalFeatures': [ 'depth-sensing' ],
-    'depthSensing': { 'usagePreference': [ 'gpu-optimized' ], 'dataFormatPreference': [] }
-  } ) );
-*/
 
   const environment = new RoomEnvironment();
   const pmremGenerator = new PMREMGenerator(renderer);
 
   scene.environment = pmremGenerator.fromScene(environment).texture;
 
-
   const xrButton = XRButton.createButton(renderer, {});
   xrButton.style.backgroundColor = 'skyblue';
   document.body.appendChild(xrButton);
 
 
-
-  // Handle input: see THREE.js webxr_ar_cones
-
-
   const onSelect = (event) => {
 
-
-    objects[0].position.set(0, 1.6, - 0.5).applyMatrix4(controller.matrixWorld);
-
+    objects[0].position.set(0, 0, - 0.5).applyMatrix4(controller.matrixWorld);
 
   }
   controller = renderer.xr.getController(0);
@@ -134,15 +69,12 @@ const init = () => {
 
 init();
 
-//
-
 
 function loadData() {
   new GLTFLoader()
     .setPath('assets/models/')
     .load('./targetA.glb', gltfReader);
 }
-
 
 function gltfReader(gltf) {
   let testModel = null;
